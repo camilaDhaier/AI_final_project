01ce42824fecf0d67ad93b332c5d9b867cc67a6d
camilaDhaier <violincamila@gmail.com>
2024-08-14 10:35:11 -0300

    added the Api integrations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+    let instructionsInput = document.querySelector("#user-instruction")
+    let apiKey = "3d1a7f2f23o09983b2643c5a0t1032fc";
+    let prompt = `Generate a French poem about ${instructionsInput.value}`;
+    let context = "";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
